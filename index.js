f92336b6c49f56f58a27629970f7a1fba88f586b
Abdullahshaikh-code <abdullahshakh569@gmail.com>
2023-09-16 12:11:09 -0700

    Production Improvements added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express=require("express");
 const app=express();
-require("./startup/logging")()
-require("./startup/config")()
-require("./startup/routes")(app)
-require("./startup/database")()
-
+require("./startup/logging")();
+require("./startup/config")();
+require("./startup/routes")(app);
+require("./startup/database")();
+require("./startup/prod")(app);
 
 const port = process.env.port  || 3000;
 const server=app.listen(port,()=>{console.log(`Server listening on ${port}`);})
--- startup/logging.js
+++ startup/logging.js
@@ -1,16 +1,16 @@
 const config=require("config");
 const winston = require('winston');
-// require('winston-mongodb');
+require('winston-mongodb');
 require("express-async-errors")
 module.exports=function(){
  winston.createLogger({
   transports: [
     new winston.transports.File({ filename: "./logs/errors.log",level:"error" }),
-    // new winston.transports.MongoDB({
-    //   db: config.get("connectingString"),
-    //   options: { useUnifiedTopology: true },
-    //   level: "info"
-    // }),
+    new winston.transports.MongoDB({
+      db: config.get("connectingString"),
+      options: { useUnifiedTopology: true },
+      level: "info"
+    }),
     new winston.transports.File({
       filename: "./logs/errors.log",
       handleExceptions: true, /* set this option to true */
